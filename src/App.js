8fa249cc0a666b5e96803b993706436966e00222
Skye Howard <tewarinerai@gmail.com>
2020-07-20 16:38:25 -0700

    added get request etc.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [iteration, setIteration] = useState(0)
+  const [message, setResponseMessage] = useState('')
+  
+  const handleSubmit = e => {
+    e.preventDefault();
+    axios.get('localhost:8081/fib/v1/iterate').then(res =>{
+      console.log(res.data);
+      setResponseMessage(res.data);
+    }).catch(err=>{
+      console.log(err);
+    })
+  }
+
+  const handleChange = e => {
+    setIteration(e.target.value);
+  }
+
   return (
     <div className="App">
       <h4>FiboServe 1.0</h4>
-      <form>
-      <p>Please enter the iteration desired.</p>
-      <input type="text" placeholder="iteration"></input>
+      <p>{message}</p>
+      <form onSubmit={handleSubmit}>
+      <p>Please enter the x for your f(x).</p>
+      <input id="iteration" type="text" onChange={handleChange}></input>
       <input type="submit" value="Submit"></input>
       </form>
     </div>
