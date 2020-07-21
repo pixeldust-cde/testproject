8c96567b6002eb6ba126f312888fb5f5335d7137
Skye Howard <tewarinerai@gmail.com>
2020-07-21 16:09:18 -0700

    App.js didn't get pushed.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React, {useState} from 'react';
-import axios from 'axios';
 import './App.css';
 
 function App() {
   const [iteration, setIteration] = useState(0)
-  const [message, setResponseMessage] = useState('')
-  
-  const handleSubmit = e => {
+  const [message, setResponseMessage] = useState('?')
+  const baseUrl = 'http://127.0.0.1:8081/fib/v1/iterate'
+
+  function handleSubmit(e){
     e.preventDefault();
-    axios.get('localhost:8081/fib/v1/iterate').then(res =>{
-      console.log(res.data);
-      setResponseMessage(res.data);
-    }).catch(err=>{
-      console.log(err);
-    })
+    setResponseMessage("...calculating...")
+    console.log(iteration);
+    const headers = {'iteration': iteration}
+    fetch(baseUrl, { headers: headers})
+    .then(response => response.text())
+    .then(data => setResponseMessage(data))
+    .catch(err => console.log(err));
   }
 
   const handleChange = e => {
@@ -23,11 +24,11 @@ function App() {
   return (
     <div className="App">
       <h4>FiboServe 1.0</h4>
-      <p>{message}</p>
       <form onSubmit={handleSubmit}>
       <p>Please enter the x for your f(x).</p>
       <input id="iteration" type="text" onChange={handleChange}></input>
       <input type="submit" value="Submit"></input>
+      <p>f(x) = {message}</p>
       </form>
     </div>
   );
